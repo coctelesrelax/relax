6a2bef488663e42c067739788fd8eed37e2a2c5d
coctelesrelax <cocteles.relax@gmail.com>
2026-02-23 13:16:47 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,58 @@
 const productos = [
   {
-    id: "vodk-tonic",
-    nombre: "Vodk-Tonic",
+    id: "vodka-tonic",
+    nombre: "Vodka Tonic",
     precio: 17500,
-    imagen: "img/vodka.jpg",
-    descripcion: "1 Minibotella Absolut + Tónica + Burbujas + Hielo"
+    imagen: "img/vodka-tonic.jpg",
+    descripcion: "1 Minibotella Absolut + Agua tónica + Burbujas + Hielo"
   },
   {
     id: "gin-selva",
-    nombre: "Gin Tonic Express Selva",
+    nombre: "Gin Tonic Selva",
     precio: 23000,
     imagen: "img/gin-selva.jpg",
-    descripcion: "1 Minibotella Ginebra Selva + Tónica + Burbujas + Hielo"
+    descripcion: "1 Minibotella Ginebra Selva + Agua tónica + Burbujas + Hielo"
+  },
+  {
+    id: "gin-whitley",
+    nombre: "Gin Tonic Whitley",
+    precio: 28000,
+    imagen: "img/gin-whitley.jpg",
+    descripcion: "1 Minibotella Ginebra Whitley + Agua tónica + Burbujas + Hielo"
   },
   {
     id: "jager-bull",
-    nombre: "Jäger-Bull",
+    nombre: "Jäger Bull",
     precio: 18000,
-    imagen: "img/jager.jpg",
-    descripcion: "1 Minibotella Jäger + Red Bull + Burbujas + Hielo"
+    imagen: "img/jager-bull.jpg",
+    descripcion: "1 Minibotella Jägermeister + Red Bull + Burbujas + Hielo"
+  },
+  {
+    id: "bourbon-grants",
+    nombre: "Kit Bourbon Cola Grant’s",
+    precio: 20500,
+    imagen: "img/bourbon-grants.jpg",
+    descripcion: "1 Minibotella Whisky Grant’s + Coca Cola + Burbujas + Hielo"
+  },
+  {
+    id: "bourbon-jack",
+    nombre: "Bourbon Cola Jack",
+    precio: 19500,
+    imagen: "img/bourbon-jack.jpg",
+    descripcion: "1 Minibotella Jack Daniel’s + Coca Cola + Burbujas + Hielo"
+  },
+  {
+    id: "cuates-vodka",
+    nombre: "Cuates - Vodka",
+    precio: 19000,
+    imagen: "img/cuates-vodka.jpg",
+    descripcion: "1 Minibotella Vodka Absolut + Lata Cuate + Burbujas + Hielo"
+  },
+  {
+    id: "cuates-grants",
+    nombre: "Cuates - Grant’s",
+    precio: 23000,
+    imagen: "img/cuates-grants.jpg",
+    descripcion: "1 Minibotella Whisky Grant’s + Lata Cuate + Burbujas + Hielo"
   }
 ];
