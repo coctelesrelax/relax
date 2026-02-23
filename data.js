bbbcc6bb7be2414257eea7d1d41c8d14ad148506
coctelesrelax <cocteles.relax@gmail.com>
2026-02-23 12:32:58 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const productos = [
+  {
+    id: "vodk-tonic",
+    nombre: "Vodk-Tonic",
+    precio: 17500,
+    imagen: "img/vodka.jpg",
+    descripcion: "1 Minibotella Absolut + Tónica + Burbujas + Hielo"
+  },
+  {
+    id: "gin-selva",
+    nombre: "Gin Tonic Express Selva",
+    precio: 23000,
+    imagen: "img/gin-selva.jpg",
+    descripcion: "1 Minibotella Ginebra Selva + Tónica + Burbujas + Hielo"
+  },
+  {
+    id: "jager-bull",
+    nombre: "Jäger-Bull",
+    precio: 18000,
+    imagen: "img/jager.jpg",
+    descripcion: "1 Minibotella Jäger + Red Bull + Burbujas + Hielo"
+  }
+];
