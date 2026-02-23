3f56112e14b9bac8fe3684c39fc6444e211236f9
coctelesrelax <cocteles.relax@gmail.com>
2026-02-23 12:43:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,61 @@ if(document.getElementById("detalle")){
     `;
   }
 }
+/* ===== MÚSICA ===== */
+function toggleMusic(){
+  const music = document.getElementById("bgMusic");
+  if(music.paused){
+    music.play();
+  }else{
+    music.pause();
+  }
+}
+
+/* ===== PARTÍCULAS ===== */
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle{
+  constructor(){
+    this.x = Math.random()*canvas.width;
+    this.y = Math.random()*canvas.height;
+    this.size = Math.random()*3+1;
+    this.speedY = Math.random()*1+0.5;
+  }
+  update(){
+    this.y += this.speedY;
+    if(this.y > canvas.height){
+      this.y = 0;
+      this.x = Math.random()*canvas.width;
+    }
+  }
+  draw(){
+    ctx.fillStyle = "rgba(0,255,255,0.7)";
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+    ctx.fill();
+  }
+}
+
+function init(){
+  for(let i=0;i<100;i++){
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  particlesArray.forEach(p=>{
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
