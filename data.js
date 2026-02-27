253ec32d909d8e202c2143d4ae313ee779a1df2c
coctelesrelax <cocteles.relax@gmail.com>
2026-02-27 14:33:06 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,77 +3,77 @@ const productos = [
     id: "vodka-tonic",
     nombre: "Vodka Tonic",
     precio: 17500,
-    imagen: "img/prod-vodka-tonic.jpg",
+    imagen: "img/vodka-tonic.jpg",
     descripcion: "1 Minibotella Absolut + Agua tónica + Burbujas + Hielo"
   },
   {
     id: "gin-selva",
     nombre: "Gin Tonic Selva",
     precio: 23000,
-    imagen: "img/prod-gin-selva.jpg",
+    imagen: "img/gin-selva.jpg",
     descripcion: "1 Minibotella Ginebra Selva + Agua tónica + Burbujas + Hielo"
   },
   {
     id: "gin-whitley",
     nombre: "Gin Tonic Whitley",
     precio: 28000,
-    imagen: "img/prod-gin-whitley.jpg",
+    imagen: "img/gin-whitley.jpg",
     descripcion: "1 Minibotella Ginebra Whitley + Agua tónica + Burbujas + Hielo"
   },
   {
     id: "jager-bull",
     nombre: "Jäger Bull",
     precio: 18000,
-    imagen: "img/prod-jager-bull.jpg",
+    imagen: "img/jager-bull.jpg",
     descripcion: "1 Minibotella Jägermeister + Red Bull + Burbujas + Hielo"
   },
   {
     id: "bourbon-grants",
     nombre: "Kit Bourbon Cola Grant’s",
     precio: 20500,
-    imagen: "img/prod-bourbon-grants.jpg",
+    imagen: "img/bourbon-grants.jpg",
     descripcion: "1 Minibotella Whisky Grant’s + Coca Cola + Burbujas + Hielo"
   },
   {
     id: "bourbon-jack",
     nombre: "Bourbon Cola Jack",
     precio: 19500,
-    imagen: "img/prod-bourbon-jack.jpg",
+    imagen: "img/bourbon-jack.jpg",
     descripcion: "1 Minibotella Jack Daniel’s + Coca Cola + Burbujas + Hielo"
   },
   {
     id: "cuates-vodka",
     nombre: "Cuates - Vodka",
     precio: 19000,
-    imagen: "img/prod-cuates-vodka.jpg",
+    imagen: "img/cuates-vodka.jpg",
     descripcion: "1 Minibotella Vodka Absolut + Lata Cuate + Burbujas + Hielo"
   },
   {
     id: "cuates-grants",
     nombre: "Cuates - Grant’s",
     precio: 23000,
-    imagen: "img/prod-cuates-grants.jpg",
+    imagen: "img/cuates-grants.jpg",
     descripcion: "1 Minibotella Whisky Grant’s + Lata Cuate + Burbujas + Hielo"
   },
   {
     id: "hechizo-violeta",
     nombre: "Hechizo - Violeta",
     precio: 40000,
-    imagen: "img/prod-hechizo-violeta.jpg",
+    imagen: "img/hechizo-violeta.jpg",
     descripcion: "1 Minibotella Ginebra whitley + Mini botella Vodka absolut + Lata Monster morado + Burbujas + Hielo"
   },
   {
     id: "perla-negra",
     nombre: "Perla - Negra",
     precio: 29000,
-    imagen: "img/prod-perla-negra.jpg",
+    imagen: "img/perla-negra.jpg",
     descripcion: "1 Minibotella Jägermaster + Mini botella Vodka absolut + Lata Monster + Burbujas + Hielo"
   },
   {
     id: "tormenta-azul",
     nombre: "Tormenta - Azul",
     precio: 30000,
-    imagen: "img/prod-tormenta-azul.jpg",
+    imagen: "img/tormenta-azul.jpg",
     descripcion: "2 Mini botella Vodka absolut + Lata Monster + Burbujas + Hielo"
   }
  ];
