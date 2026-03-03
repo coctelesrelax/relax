81aec4930b4014188e79e3449d846d28a51753c4
coctelesrelax <cocteles.relax@gmail.com>
2026-03-03 11:06:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -151,3 +151,21 @@ counters.forEach(counter => {
 
   updateCount();
 });
+
+/* =========================================
+   SCROLL REVEAL
+========================================= */
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const revealTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if(revealTop < windowHeight - revealPoint){
+      el.classList.add("active");
+    }
+  });
+});
