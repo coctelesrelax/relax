b8259a523ba206a429478f17bf03027b81999911
coctelesrelax <cocteles.relax@gmail.com>
2026-03-03 10:55:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -127,3 +127,27 @@ if(canvas){
 }
 
 }); 
+
+/* =========================================
+   CONTADOR ANIMADO
+========================================= */
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const increment = target / 100;
+
+    if(count < target){
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
